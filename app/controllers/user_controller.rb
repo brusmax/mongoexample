bf6ab42fc80b0f399142dd5ff7b2be5bfb0c8b47
Sala de juntas <Salajuntas@Salas-Mac-mini.local>
2012-12-14 17:18:11 -0600

    close #1 \n se edito

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,13 @@
 class UserController < ApplicationController
   def index
   	user = User.new
-	user.username = "mike"
-	user.firstname = "Mike"
-	user.lastname = "Boby"
-	user.age = 44
+	user.username = "Brusmax"
+	user.firstname = "Rod"
+	user.lastname = "Quiue"
+	user.age = 15
 	user.save
 
-	user = User.where( username: "mike")
+	user = User.where( username: "Brusmax")
 	abort(user.to_json)
   end
 end
